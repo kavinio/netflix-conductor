c5f78e7ee4da5854b9ae0b35b68c2ea98ec54385
Kavin Anbazhagan <Kavin.Anbazhagan@phoenix.edu>
2019-10-01 14:18:02 -0700

    get redisConfigEndpoint from env var

--- redis-persistence/src/main/java/com/netflix/conductor/jedis/RedisClusterJedisProvider.java
+++ redis-persistence/src/main/java/com/netflix/conductor/jedis/RedisClusterJedisProvider.java
@@ -34,24 +34,35 @@ public class RedisClusterJedisProvider implements Provider<JedisCommands> {
     public JedisCommands get() {
         // FIXME This doesn't seem very safe, but is how it was in the code this was moved from.
         // this should give redisConfigEndpoint
+        // hostSupplier is created in ConfigurationHostSupplierProvider
         Host host = new ArrayList<Host>(hostSupplier.getHosts()).get(0);
         GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
         poolConfig.setMinIdle(5);
         poolConfig.setMaxTotal(1000);
         try {
-            InetAddress[] allAddress = InetAddress.getAllByName(host.getHostName());
+            String redisConfigEndpoint = host.getHostName();
+            if(redisConfigEndpoint.contains("$")){
+                redisConfigEndpoint = System.getenv(redisConfigEndpoint.replace("$","").replace("{","").replace("}",""));
+            }
+            System.out.println("#################################################");
+            System.out.println("#################################################");
+            System.out.println("REDIS_CONFIG_ENDPOINT = " + redisConfigEndpoint);
+            InetAddress[] allAddress = InetAddress.getAllByName(redisConfigEndpoint);
             Set<HostAndPort> allNodes = new HashSet<HostAndPort>();
             // this will collect ip of all the nodes
             for(InetAddress address : allAddress) {
+                System.out.println("Adding host node " + address.getHostAddress() + ":" + host.getPort());
                 allNodes.add(new HostAndPort(address.getHostAddress(), host.getPort()));
             }
             if(!allNodes.isEmpty()) {
+                System.out.println("Connecting to multiple host nodes......");
                 return new JedisCluster(allNodes, poolConfig);
             }
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
 
+        System.out.println("Connecting to SINGLE host node " + host.getHostName() + ":" + host.getPort());
         return new JedisCluster(new HostAndPort(host.getHostName(), host.getPort()), poolConfig);
     }
 }
